7867366b0ab8d3276e6c10cf3bda712859b526ba
Gideon Tobing <gideon.tobing@gmail.com>
2025-12-26 01:37:51 +1100

    add contact sheet

---
+++ composeApp/src/commonMain/kotlin/com/gt/cmp_contactlist/contacts/presentation/components/AddContactSheet.kt
@@ -0,0 +1,125 @@
+package com.gt.cmp_contactlist.contacts.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.gt.cmp_contactlist.contacts.domain.Contact
+import com.gt.cmp_contactlist.contacts.presentation.ContactListEvent
+import com.gt.cmp_contactlist.contacts.presentation.ContactListState
+import com.gt.cmp_contactlist.core.presentation.BottomSheetFromWish
+
+@Composable
+fun AddContactSheet(
+    state: ContactListState,
+    newContact: Contact?,
+    isOpen: Boolean,
+    onEven: (ContactListEvent) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    BottomSheetFromWish(
+        visible = isOpen,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopStart
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(Modifier.height(60.dp))
+
+                if (newContact?.photoBytes == null) {
+                    Box(
+                        modifier = Modifier
+                            .size(150.dp)
+                            .clip(RoundedCornerShape(40))
+                            .background(MaterialTheme.colorScheme.secondaryContainer)
+                            .clickable {
+                                onEven(ContactListEvent.OnAddPhotoClicked)
+                            }
+                            .border(
+                                width = 1.dp,
+                                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                                shape = RoundedCornerShape(40)
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.Add,
+                            contentDescription = "Add Photo",
+                            tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
+                } else {
+                    ContactPhoto(
+                        contact = newContact,
+                        modifier = Modifier
+                            .size(150.dp)
+                            .clickable {
+                                onEven(ContactListEvent.OnAddPhotoClicked)
+                            }
+                    )
+                }
+                Spacer(Modifier.height(16.dp))
+            }
+
+            IconButton(
+                onClick = {
+                    onEven(ContactListEvent.DismissContact)
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Close,
+                    contentDescription = "Close"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ContactTextField(
+    value: String,
+    placeholder: String,
+    error: String?,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+        OutlinedTextField(
+            value = value,
+            placeholder = { Text(text = placeholder) },
+            onValueChange = onValueChanged,
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier.fillMaxWidth(),
+        )
+        if (error != null) {
+            Text(text = error, color = MaterialTheme.colorScheme.error)
+        }
+    }
+}
